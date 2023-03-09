a7afe2b8158d13077a72570f3ee6bd13a3a7f9e3
wenkang124 <wenkang@etctech.com.my>
2023-03-09 18:23:40 +0800

    notification time format

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -30,6 +30,8 @@ class NotificationController extends Controller
     {
 
         $notification = Notification::where('id', $id)->firstOrFail();
+        $notification->data = json_decode($notification->data);
+        $notification->data['display_date'] = $notification->display_date;
 
         return $this->__apiSuccess(
             'Retrieve Successful.',

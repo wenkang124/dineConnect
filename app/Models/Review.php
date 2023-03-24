c377972e910e6a0a8c666c9e45ddb2d4718cf52f
ChingVoon <chingvoonc@gmail.com>
2023-03-25 02:49:02 +0800

    withTrashed ?

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -60,7 +60,7 @@ class Review extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
     
     public function merchant()
